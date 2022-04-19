c10cfd4916026b34862864cf0639b33a1cc6cedd
Velizar <31567122+VelizarBG@users.noreply.github.com>
2022-04-19 18:55:29 +0300

    Work around server suggestion issue and partially extend functionality to servers

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Suggestion Tweaker
 
-This is a client mod which changes the way identifiers(such as functions, item modifiers, loot tables, etc.) and other things(scoreboard objective criteria, opped players, datapacks, and others) are picked in the suggestions tab when writing a command in chat.
+This is a client and server mod which changes the way suggestions are picked in the suggestions tab when writing a command in chat.
+
+## Improved filtering
 
 Vanilla usually starts comparing the input with the start of the suggestion candidate and discards if it doesn't match. In some cases it checks for a `.` or `_` and then the input. This can often be annoying when trying to search for something because it is too strict. This mod changes that, so now any entry containing the input anywhere inside it is included.
+
+## Server and client
+
+Suggestions can be separated into two groups - ones that are filtered on the server(based on the client's input) and ones that aren't. The former are things such as functions, loot tables, item modifiers, predicates, and a few others. The latter are things such as scoreboard criteria, colors, blocks, and others. Since the former's filtering is controlled by the server, a workaround is established to let the client use the new way of filtering suggestions independent of the server. This mod is also a server mod and provides better suggestions even for clients without the mod(but only the former group of suggestions).
 
 ## Examples
 
---
+++ src/main/java/velizarbg/suggestion_tweaker/mixins/ClientCommandSourceMixin.java
@@ -0,0 +1,48 @@
+package velizarbg.suggestion_tweaker.mixins;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.context.StringRange;
+import com.mojang.brigadier.suggestion.Suggestion;
+import com.mojang.brigadier.suggestion.Suggestions;
+import net.minecraft.client.network.ClientCommandSource;
+import net.minecraft.command.CommandSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A workaround to the sorting of server-specific suggestions being outsourced to the server.
+ */
+@Mixin(ClientCommandSource.class)
+public class ClientCommandSourceMixin {
+	private String currentInput;
+
+	/**
+	 * In order to fetch all possible suggestions the last argument needs to be empty.
+	 */
+	@Redirect(method = "getCompletions", at = @At(target = "Lcom/mojang/brigadier/context/CommandContext;getInput()Ljava/lang/String;", value = "INVOKE"))
+	private String processInput(CommandContext<?> context) {
+		String input = context.getInput();
+		int lastSpaceIndex = input.lastIndexOf(' ');
+		currentInput = input.substring(lastSpaceIndex + 1);
+		return input.substring(0, lastSpaceIndex + 1);
+	}
+
+	@ModifyVariable(method = "onCommandSuggestions", at = @At(target = "Ljava/util/concurrent/CompletableFuture;complete(Ljava/lang/Object;)Z", value = "INVOKE", shift = At.Shift.BEFORE), index = 2, argsOnly = true)
+	private Suggestions filterSuggestions(Suggestions suggestions) {
+		int start = Integer.MAX_VALUE;
+		int end = Integer.MIN_VALUE;
+		List<Suggestion> suggestionList = new ArrayList<>();
+		for (Suggestion suggestion : suggestions.getList()) {
+			if (!CommandSource.shouldSuggest(currentInput, suggestion.getText())) continue;
+			suggestionList.add(suggestion);
+			start = Math.min(suggestion.getRange().getStart(), start);
+			end = Math.max(suggestion.getRange().getEnd(), end);
+		}
+		return new Suggestions(new StringRange(start, end), suggestionList);
+	}
+}
