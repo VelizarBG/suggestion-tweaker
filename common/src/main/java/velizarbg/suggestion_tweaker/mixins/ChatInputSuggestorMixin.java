eebaaf04af61fa2324403865b3143837ee30c984
Velizar <31567122+VelizarBG@users.noreply.github.com>
2022-08-03 20:28:40 +0300

    Update to 1.19.1

--- common/src/main/java/velizarbg/suggestion_tweaker/mixins/ChatInputSuggestorMixin.java
+++ common/src/main/java/velizarbg/suggestion_tweaker/mixins/ChatInputSuggestorMixin.java
@@ -1,7 +1,7 @@
 package velizarbg.suggestion_tweaker.mixins;
 
 import com.mojang.brigadier.suggestion.Suggestion;
-import net.minecraft.client.gui.screen.CommandSuggestor;
+import net.minecraft.client.gui.screen.ChatInputSuggestor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -10,8 +10,8 @@ import static velizarbg.suggestion_tweaker.Constants.config;
 
 import java.util.Locale;
 
-@Mixin(CommandSuggestor.class)
-public class CommandSuggestorMixin {
+@Mixin(ChatInputSuggestor.class)
+public class ChatInputSuggestorMixin {
 	@Redirect(method = "sortSuggestions", at = @At(value = "INVOKE", target = "Lcom/mojang/brigadier/suggestion/Suggestion;getText()Ljava/lang/String;"))
 	private String tryLowerCaseSuggestion(Suggestion suggestion) {
 		return
